9b3ee0cd3bb18770be0ea287c90b5e085e143135
ivan zinchenko <evandersar@ukr.net>
2016-11-16 17:03:37 +0200

    some stylling

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,20 +1,23 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
 import { HeroService } from './hero.service';
-import { Hero } from "./hero";
 
 import 'rxjs/add/operator/switchMap';
+import { Hero } from "./hero";
 
 
 @Component({
   moduleId: module.id,
   selector: 'my-hero-detail',
-  templateUrl: 'hero-detail.component.html'
+  templateUrl: 'hero-detail.component.html',
+  styleUrls: [ 'hero-detail.component.css' ]
 })
 
 export class HeroDetailComponent implements OnInit {
+
+  hero: Hero;
 
   constructor(
       private heroService: HeroService,
@@ -32,5 +35,4 @@ export class HeroDetailComponent implements OnInit {
     this.location.back();
   }
 
-  @Input() hero: Hero;
 }
